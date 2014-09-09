198773b491f40d88997923b1502b4c13dbeb65fc
Ian D. Scott <ian@perebruin.com>
2014-09-08 17:16:55 -0700

    Move much of Wiki class to new Article class

--- src/wikicurses/wiki.py
+++ src/wikicurses/wiki.py
@@ -6,9 +6,6 @@ from wikicurses.htmlparse import ExcerptHTMLParser
 from wikicurses import wikis
 
 class Wiki(object):
-    result = None
-    page = None
-
     def __init__(self, url):
         self.siteurl = url
 
@@ -19,16 +16,23 @@ class Wiki(object):
 
         url = self.siteurl + urllib.parse.urlencode(data)
         # open url, read content (bytes), convert in string via decode()
-        self.result = json.loads(urllib.request.urlopen(url).read().decode('utf-8'))
+        result = json.loads(urllib.request.urlopen(url).read().decode('utf-8'))
+        return _Article(result)
+
+
+class _Article(object):
+    def __init__(self, result):
+        self.result = result
         # In python 3 dict_keys are not indexable, so we need to use list()
         key = list(self.result['query']['pages'])[0][:]
         self.page = self.result['query']['pages'][key]
+
 
     @property
     def title(self):
         return self.page['title']
 
-    def __get_extract(self):
+    def _get_extract(self):
         """ Get extract """
         try:
             parser = ExcerptHTMLParser()
@@ -41,7 +45,7 @@ class Wiki(object):
             return {'':'No wikipedia page for that title.\n'
                    'Wikipedia search titles are case sensitive.'}
 
-    def __get_external_links(self):
+    def _get_external_links(self):
         """ Get external links """
         try:
             offset = self.result['query-continue']['extlinks']['eloffset']
@@ -56,7 +60,7 @@ class Wiki(object):
         except KeyError:
             pass
 
-    def __get_interwiki_links(self):
+    def _get_interwiki_links(self):
         """ Inter wiki links """
         try:
             iwlinks = self.page['iwlinks']
@@ -70,7 +74,7 @@ class Wiki(object):
                 continue
         return output
 
-    def __get_images(self):
+    def _get_images(self):
         """ Get images urls """
 
         image_url = "http://en.wikipedia.org/wiki/"
@@ -86,11 +90,11 @@ class Wiki(object):
             pass
 
     def get_content(self):
-        sections = self.__get_extract()
+        sections = self._get_extract()
         sections.update({
-            'Images\n':self.__get_images(),
-            '\nExternal links\n':self.__get_external_links(),
-            '\nInterwiki links\n':self.__get_interwiki_links()
+            'Images\n':self._get_images(),
+            '\nExternal links\n':self._get_external_links(),
+            '\nInterwiki links\n':self._get_interwiki_links()
             })
         return sections
 
