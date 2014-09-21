34f67f104c7c9211af1c79bc547d80970f8c09a0
Ian D. Scott <ian@perebruin.com>
2014-09-21 15:03:30 -0700

    Move code out of createWindow() that does not have to be there

--- src/wikicurses/ui.py
+++ src/wikicurses/ui.py
@@ -1,5 +1,22 @@
 import urwid
 
+screen = urwid.raw_display.Screen() 
+screen.register_palette_entry('h1', 'yellow,bold', '')
+screen.register_palette_entry('h2', 'underline', '')
+screen.register_palette_entry('h', 'underline', '')
+#screen.register_palette_entry('italic', 'italics', '') #No italics option?
+screen.register_palette_entry('bold', 'bold', '')
+screen.register_palette_entry('bolditalic', 'bold', '')
+screen.register_palette_entry('blockquote', 'dark gray', '')
+
+def keymapper(input):
+    #TODO: Implement gg and G
+    if input == 'q':
+        raise  urwid.ExitMainLoop
+    else:
+       return False
+    return True
+
 def createWindow(title, content):
     widgets = [urwid.Text([('h1', title), '\n'], align="center")]
     for title, content in content.items():
@@ -12,23 +29,6 @@ def createWindow(title, content):
     pager._command_map['j'] = 'cursor down'
     pager._command_map['ctrl b'] = 'cursor page up'
     pager._command_map['ctrl f'] = 'cursor page down'
-
-    screen = urwid.raw_display.Screen() 
-    screen.register_palette_entry('h1', 'yellow,bold', '')
-    screen.register_palette_entry('h2', 'underline', '')
-    screen.register_palette_entry('h', 'underline', '')
-    #screen.register_palette_entry('italic', 'italics', '') #No italics option?
-    screen.register_palette_entry('bold', 'bold', '')
-    screen.register_palette_entry('bolditalic', 'bold', '')
-    screen.register_palette_entry('blockquote', 'dark gray', '')
-
-    def keymapper(input):
-        #TODO: Implement gg and G
-        if input == 'q':
-            raise  urwid.ExitMainLoop
-        else:
-            return False
-        return True
 
     loop = urwid.MainLoop(pager, screen=screen, handle_mouse=False,
                          unhandled_input=keymapper)
