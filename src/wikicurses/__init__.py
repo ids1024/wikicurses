2fea7b008336e1960efb375c63a4cc14053bc590
Ian D. Scott <ian@perebruin.com>
2014-09-22 16:34:51 -0700

    Create BitEnum class for bitfields

--- src/wikicurses/__init__.py
+++ src/wikicurses/__init__.py
@@ -1,8 +1,12 @@
 import pkgutil
-from enum import IntEnum
+from enum import Enum
 
 _data = pkgutil.get_data('wikicurses', 'interwiki.list').decode()
 wikis = dict([i.split('|')[0:2] for i in _data.splitlines() if i[0]!='#'])
 
-class formats(IntEnum):
-    i, b, blockquote = (1<<i for i in range(3))
+class BitEnum(int, Enum):
+    def __new__(cls, *args):
+        value = 1 << len(cls.__members__)
+        return int.__new__(cls, value)
+
+formats = BitEnum("formats", "i b blockquote")
