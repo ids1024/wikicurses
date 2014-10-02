1d09bad155b65f3c4467c8760821992d115e4020
Ian D. Scott <ian@perebruin.com>
2014-10-02 16:38:26 -0700

    Move header out of pager

--- src/wikicurses/ui.py
+++ src/wikicurses/ui.py
@@ -4,7 +4,7 @@ from wikicurses import formats
 #TODO: Turn this into a class?
 
 screen = urwid.raw_display.Screen() 
-screen.register_palette_entry('h1', 'bold', '')
+screen.register_palette_entry('h1', 'bold', 'dark blue')
 screen.register_palette_entry('h2', 'underline', '')
 screen.register_palette_entry('h', 'underline', '')
 
@@ -16,8 +16,11 @@ for x in range(1, sum(formats) + 1):
 
 widgets = urwid.listbox.SimpleFocusListWalker([])
 widgetnames = OrderedDict()
-
 pager = urwid.ListBox(widgets)
+
+header = urwid.Text('Wikicurses', align='center')
+mainwidget = urwid.Frame(pager, urwid.AttrMap(header, 'h1'))
+
 pager._command_map['k'] = 'cursor up'
 pager._command_map['j'] = 'cursor down'
 pager._command_map['ctrl b'] = 'cursor page up'
@@ -27,7 +30,7 @@ def selectWidget(radio_button, new_state, args):
     if new_state:
         widget = args[0]
         index = widgets.index(widget)
-        loop.widget = pager
+        loop.widget = mainwidget
         widgets.set_focus(index)
 
 def keymapper(input):
@@ -35,38 +38,41 @@ def keymapper(input):
     if input == 'q':
         raise  urwid.ExitMainLoop
     elif input == 'c':
-        if loop.widget is pager:
+        if loop.widget is mainwidget:
+            current = None
             for widget in widgetnames.values():
                 if widgets.focus >= widgets.index(widget):
                     current = widget
                 else:
                     break
             radiobuttons = []
+            #Go to first widget when title is selected
+            urwid.RadioButton(radiobuttons, header.text, state=(not current),
+                    on_state_change=selectWidget, user_data=[widgets[0]])
+
             for name, widget in widgetnames.items():
                 urwid.RadioButton(radiobuttons, name, state=(current==widget),
                         on_state_change=selectWidget, user_data=[widget])
             toc = urwid.LineBox(urwid.ListBox(radiobuttons))
-            overlay = urwid.Overlay(toc, pager,
+            overlay = urwid.Overlay(toc, mainwidget,
                 'center', ('relative', 50), 'middle', ('relative', 50))
             loop.widget = overlay
         else:
-            loop.widget = pager
+            loop.widget = mainwidget
     else:
        return False
     return True
 
-loop = urwid.MainLoop(pager, screen=screen, handle_mouse=False,
+loop = urwid.MainLoop(mainwidget, screen=screen, handle_mouse=False,
                      unhandled_input=keymapper)
 
 def setContent(title, content):
     widgets.clear()
     widgetnames.clear()
-    h1 = urwid.Text([('h1', title), '\n'], align="center")
-    widgets.append(h1)
-    widgetnames[title] = h1
+    header.set_text(title)
     for title, content in content.items():
         if title:
-            h2 = urwid.Text([('h1', title), '\n'], align="center")
+            h2 = urwid.Text([('h2', title), '\n'], align="center")
             widgets.append(h2)
             widgetnames[title] = h2
         widgets.append(urwid.Text(content))
